b0a3e479be09b629db6b3ea85f97f4c204050f24
knappema <knappema@gmail.com>
2014-11-22 17:52:59 +0100

    inital add of the wpf application is complete added RelayCommand for buttons modified view to show two images modified OcrManager to read images

--- ORC_Projekt/BL/OcrManager.cs
+++ ORC_Projekt/BL/OcrManager.cs
@@ -6,13 +6,110 @@ using System.Drawing;
 
 namespace ORC_Projekt.BL
 {
-    public class OcrManager
+    public class OcrManager : OcrManagerBase
     {
-        Bitmap _originalImage;
+        private readonly Bitmap _originalImage;
+        /// <summary>
+        /// Current image, which can be viewed in frontend.
+        /// </summary>
+        private Bitmap _currentImage;
+        /// <summary>
+        /// Current image, which will be used for modifications
+        /// </summary>
+        private Bitmap _currentWorkingImage;
+        private string _resultText;
 
         public OcrManager(string fileName)
         {
             _originalImage = new Bitmap(fileName);
         }
+
+        #region Properties
+
+        public Bitmap OriginalImage
+        {
+            get
+            {
+                return _originalImage;
+            }
+        }
+
+        public Bitmap CurrentImage
+        {
+            get
+            {
+                return _currentImage;
+            }
+            private set
+            {
+                if (_currentImage != value)
+                {
+                    _currentImage = value;
+                    OnPropertyChanged("CurrentImage");
+                }
+            }
+        }
+
+        public String ResultText
+        {
+            get
+            {
+                return _resultText;
+            }
+            private set
+            {
+                if (_resultText != value)
+                {
+                    _resultText = value;
+                    OnPropertyChanged("ResultText");
+                }
+            }
+        }
+
+        #endregion
+
+        #region Publics
+
+        /// <summary>
+        /// Starts the ocr.
+        /// </summary>
+        public void Start()
+        {
+            //test
+            CurrentImage = OriginalImage;
+            ResultText = "Cheese";
+
+
+            PreProcessing();
+            Ocr();
+            PostProcessing();
+        }
+
+        #endregion
+
+        #region Privates
+
+        /// <summary>
+        /// All methods for pre-processing
+        /// </summary>
+        private void PreProcessing()
+        {
+        }
+
+        /// <summary>
+        /// All methods for (in the moment) one specific ocr 
+        /// </summary>
+        private void Ocr()
+        {
+        }
+
+        /// <summary>
+        /// All methods for post-processing
+        /// </summary>
+        private void PostProcessing()
+        {
+        }
+
+        #endregion
     }
 }
--- ORC_Projekt/ViewModel/MainWindowViewModel.cs
+++ ORC_Projekt/ViewModel/MainWindowViewModel.cs
@@ -2,14 +2,164 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Input;
+using ORC_Projekt.Commands;
+using System.IO;
+using System.Windows.Forms;
+using ORC_Projekt.BL;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.ComponentModel;
+using System.Windows.Interop;
+using System.Drawing;
 
 namespace ORC_Projekt.ViewModel
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : ViewModelBase
     {
+        private readonly ICommand _selectFileCommand;
+        private readonly ICommand _startOcrCommand;
+        private OcrManager _ocrManager;
+
         public MainWindowViewModel()
         {
-
+            _selectFileCommand = new RelayCommand(ExecuteSelectFile, CanExecuteSelectFile);
+            _startOcrCommand = new RelayCommand(ExecuteStartOcr, CanExecuteStartOcr);
         }
+
+        #region Properties
+
+        public ImageSource OriginalImage
+        {
+            get
+            {
+                if (_ocrManager != null)
+                {
+                    Bitmap bitmap = _ocrManager.OriginalImage;
+
+                    BitmapSource source = Imaging.CreateBitmapSourceFromHBitmap(bitmap.GetHbitmap(), 
+                        IntPtr.Zero, System.Windows.Int32Rect.Empty, BitmapSizeOptions.FromWidthAndHeight(bitmap.Width, bitmap.Height));
+                    return source;
+                }
+                return null;
+            }
+        }
+
+        public ImageSource CurrentImage
+        {
+            get
+            {
+                if (_ocrManager != null)
+                {
+                    Bitmap bitmap = _ocrManager.CurrentImage;
+
+                    BitmapSource source = Imaging.CreateBitmapSourceFromHBitmap(bitmap.GetHbitmap(),
+                        IntPtr.Zero, System.Windows.Int32Rect.Empty, BitmapSizeOptions.FromWidthAndHeight(bitmap.Width, bitmap.Height));
+                    return source;
+                }
+                return null;
+            }
+        }
+
+        public String ResultText
+        {
+            get
+            {
+                if (_ocrManager != null)
+                {
+                    return _ocrManager.ResultText;
+                }
+                return String.Empty;
+            }
+        }
+            
+
+
+        #endregion
+
+        #region Commands
+
+        public ICommand SelectFileCommand
+        {
+            get
+            {
+                return _selectFileCommand;
+            }
+        }
+
+        public ICommand StartOcrCommand
+        {
+            get
+            {
+                return _startOcrCommand;
+            }
+        }
+
+
+        #endregion
+
+        #region CommandHelper
+
+        private bool CanExecuteSelectFile(object o)
+        {
+            // TODO is ocr running ???
+            return true;
+        }
+
+        private void ExecuteSelectFile(object o)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png"; ;
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string fileName = openFileDialog1.FileName;
+                    if (fileName != null)
+                    {
+                        createOcrManager(fileName);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private bool CanExecuteStartOcr(object o)
+        {
+            return _ocrManager != null;
+        }
+
+        private void ExecuteStartOcr(object o)
+        {
+            _ocrManager.Start();
+        }
+
+        #endregion
+
+
+        #region Privates
+
+        private void createOcrManager(string fileName)
+        {
+            _ocrManager = null;
+            _ocrManager = new OcrManager(fileName);
+            _ocrManager.PropertyChanged += new PropertyChangedEventHandler(PropertyHasChanged);
+            OnPropertyChanged("OriginalImage");
+        }
+
+        private void PropertyHasChanged(object o, PropertyChangedEventArgs e)
+        {
+            OnPropertyChanged(e.PropertyName);
+        }
+
+        #endregion
     }
 }
