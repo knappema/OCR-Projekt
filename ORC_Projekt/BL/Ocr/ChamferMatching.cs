0609f1ce066fc43f0d70a0e8587b68ed1b2d1204
knappema <knappema@gmail.com>
2014-11-26 12:23:39 +0100

    added inital class for chamfer matching

---
+++ ORC_Projekt/BL/Ocr/ChamferMatching.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ORC_Projekt.BL.Ocr
+{
+    class ChamferMatching
+    {
+        private readonly uint[,] _distanceMap;
+        private readonly List<int> _resultList;
+
+        public ChamferMatching(uint[,] distanceMap)
+        {
+            _distanceMap = distanceMap;
+            _resultList = new List<int>();
+        }
+
+        public List<int> ResultList
+        {
+            get
+            {
+                return _resultList;
+            }
+        }
+
+        #region Publics
+
+        public void Start()
+        {
+        }
+
+        #endregion
+    }
+}
